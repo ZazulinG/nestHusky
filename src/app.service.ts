539f3b9654f7df227b76b01e260cfeb856e3281b
George <zazul@yandex.ru>
2022-07-12 16:50:26 +0300

    commit huskynest

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,7 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
-  getOne():                      string{
-    return                          '321321321'
+  getOne(): string {
+    return '321321321';
   }
 }
